40753f7be4626e072500eb8dce6ae2d816185f39
Muhammad Khan <muhammad.khan33@student.qcc.cuny.edu>
2020-11-23 21:18:12 -0500

    Session02.py of Array added

---
+++ Session02.py
@@ -0,0 +1,20 @@
+# An array can hold many values under a single name
+# you can access the values by referring to an index number
+
+# Get the value of the first array item
+cars = ["Ford", "Tesla", "BMW"]
+x = cars[0]
+print(x)
+
+# To modify the value of the first item
+cars[0] = "Benz"
+print(cars)
+
+# Using a for loop
+for x in cars:
+    print(x)
+
+# Add an element to the cars array using Append() method
+cars.append("Audi")
+print(cars)
+
